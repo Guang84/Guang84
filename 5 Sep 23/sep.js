8e6d5663768459e55d468b8bd177848a7a905b05
Guang84 <144978830+Guang84@users.noreply.github.com>
2023-09-15 12:36:41 +0530

    Add files via upload

---
+++ 5 Sep 23/sep.js
@@ -0,0 +1,31 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+    var currentScrollpos = window.pageYOffset;
+    if (prevScrollpos > currentScrollpos){
+        document.getElementById("topbar").style.top = "0";
+    }
+    else{
+        document.getElementById("topbar").style.top = "-50px";
+    }
+    prevScrollpos = currentScrollpos;
+}
+function Clublist()
+{
+    let input = document.getElementById('searchbar').value
+    input = input.toLowerCase();
+    let x= document.getElementsByTagName('td');
+    for (i = 0; i < x.length; i++)
+    {
+        if(!x[i].innerHTML.toLowerCase().includes(input))
+        {
+            x[i].style.display="none";
+        }
+        else
+        {
+            x[i].style.display="list-item";
+        }
+    }
+}
+function adlog()
+{
+}
